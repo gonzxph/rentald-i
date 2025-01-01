a60fe9c2de2bebeb4f9e27ba70973180a7952316
Ronnie187 <133963808+Ronnie187@users.noreply.github.com>
2025-01-01 13:03:48 +0800

    added comment about port number in db_conn

--- admin/db_conn.php
+++ admin/db_conn.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "carrental_db";
 
+//E change lang ng port number og 3306(or onsa imo port) kay kanang 3308 akoa ng port number
 $conn = new mysqli($servername, $username, $password, $dbname, 3308);
 
 if ($conn->connect_error) {
